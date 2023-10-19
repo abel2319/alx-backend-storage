708b1eefe2a44bb0feca4553251acb7bf0ddaee8
abel2319 <abelbakary@gmail.com>
2023-10-19 13:20:02 +0100

    feat(task1):task 0 and readme

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pythoni3
+"""0. Writing strings to Redis
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Class Cache for saving
+    """
+    def __init__(self):
+        """initialization
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data to redis data base
+        with an uuid as key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
