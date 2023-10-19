5144b4d7be5ddcb6c9ceab42384f3374087b546c
abel2319 <abelbakary@gmail.com>
2023-10-19 20:35:53 +0100

    fix(task1) task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """0. Writing strings to Redis
 """
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
@@ -22,3 +22,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, bytes, int, float]:
+        """Get method for cache
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_int(data: bytes) -> int:
+        """convert byte to integer
+        """
+
+    def get_str(self, data: bytes) -> str:
+        """convert byte to string
+        """
+        return data.decode('utf-8')
--- 0x02-redis_basic/test.py
+++ 0x02-redis_basic/test.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-"""
-Main file
-"""
-import redis
-
-Cache = __import__('exercise').Cache
-
 cache = Cache()
-
-data = b"hello"
-key = cache.store(data)
-print(key)
-
-local_redis = redis.Redis()
-print(local_redis.get(key))
-
 
 TEST_CASES = {
     b"foo": None,
